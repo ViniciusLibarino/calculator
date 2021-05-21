a5434aadcaf46846a3e4b31ed2fff54ea3d0a089
Vinicius Libarino <viny.liba013@gmail.com>
2021-05-21 20:56:17 -0300

    estilizaçao componente button

---
+++ src/components/Button/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components/native";
+
+export const Num = styled.TouchableOpacity`
+    height: 17%;
+    width: 20%;
+    background-color: #5C6F66;
+    justify-content: center;
+    align-items: center;
+    margin: 9px;
+    border-radius: 16px;
+`
+
+export const Text = styled.Text `
+    color: #BDBEC0;
+    font-size: 25px;
+    font-weight: 400;
+`
