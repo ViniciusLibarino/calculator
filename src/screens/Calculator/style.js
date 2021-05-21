a46dc7c46f85fe2a99b0b386bd50bc197ca6029c
Vinicius Libarino <viny.liba013@gmail.com>
2021-05-21 20:57:30 -0300

    estiizaçao calculator

---
+++ src/screens/Calculator/style.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View `
+    background-color: #060709;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: 100%;
+`
+
+export const Calc = styled.View `
+    width: 100%;
+    height: 70%;
+    flex-direction: row;
+    flex-wrap: wrap;
+    background-color: #5F756B;
+    padding: 3%;
+    border-top-right-radius: 32px;
+    border-top-left-radius: 32px;
+`
