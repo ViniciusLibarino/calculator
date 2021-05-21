ca75c4020286e068b1876dbe0a35472fc53de013
Vinicius Libarino <viny.liba013@gmail.com>
2021-05-21 20:56:52 -0300

    estilizaçao componente display

---
+++ src/components/Display/style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+
+export const Label = styled.View `
+    height: 30%;
+    width: 100%;
+    justify-content: flex-end;
+    padding: 10%;
+    align-items: flex-end;
+`
+export const Text = styled.Text `
+    color: #BDBEC0;
+    font-size: 50px;
+    font-weight: bold;
+`
